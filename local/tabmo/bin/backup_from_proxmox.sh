dc2286b324045b96dda75e09f6ff59716fe0d921
badele <bruno.adele@jesuislibre.org>
2016-01-06 14:14:04 +0100

    Add progress syncrhonisation

--- local/tabmo/bin/backup_from_proxmox.sh
+++ local/tabmo/bin/backup_from_proxmox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ "$TABMO_PROXMOX_BACKUP" != "" ]; then
-	rsync -avr $TABMO_PROXMOX_BACKUP /mnt/tabmobackup/Backup/OVH/Proxmox/
+	rsync -avr --progress $TABMO_PROXMOX_BACKUP /mnt/tabmobackup/Backup/OVH/Proxmox/
 fi
 
