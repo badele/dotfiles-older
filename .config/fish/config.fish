34f8b30f27e65e59d41b72341faafb66db81455a
badele <bruno.adele@jesuislibre.org>
2016-02-06 22:37:26 +0100

    Update i3 config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,18 +2,8 @@ set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
-# Path to Oh My Fish install.
-#set -gx OMF_PATH "/home/badele/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/badele/.config/omf"
-
-# Load oh-my-fish configuration.
-#source $OMF_PATH/init.fish
-
 # Misc
-#set --erase fish_greeting
-#set --global --export LANG fr_FR.UTF-8
+set --global --export LANG fr_FR.UTF-8
 
 # Start X at login
 if status --is-login
