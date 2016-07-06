9a65932e15d7e77a341b4b021774312fce9e19b0
badele <bruno@adele.im>
2016-07-06 20:29:51 +0200

    Use bobthefish instead shellder

--- commons/user/.config/fish/functions/fish_prompt.fish
+++ commons/user/.config/fish/functions/fish_prompt.fish
@@ -1,17 +1,1 @@
-function fish_prompt
-	set -g RETVAL $status
-    prompt_status
-    prompt_virtual_env
-    prompt_user
-    prompt_dir
-    available hg
-    and prompt_hg
-    available git
-    and prompt_git
-    available svn
-    and prompt_svn
-    prompt_finish
-    if set -q VIRTUAL_ENV
-        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-    end
-end
+/home/badele/.config/fisherman/bobthefish/fish_prompt.fish
