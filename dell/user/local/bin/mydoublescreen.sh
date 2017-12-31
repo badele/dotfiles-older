e4e77064a75f709004ce9c924f56acde92be3ab7
badele <bruno@adele.im>
2017-12-31 13:59:50 +0100

    Revert "Update screen layout"
    This reverts commit 24434291cde902884b99dbfb1153e115dc3a71ff.

--- dell/user/local/bin/mydoublescreen.sh
+++ dell/user/local/bin/mydoublescreen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 1680x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --mode 1680x1050 --pos 0x0 --rotate normal --output DP1-2 --off --output DP1-1 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --mode 1680x1050 --pos 1366x0 --rotate normal --output DP1-2 --off --output DP1-1 --off
